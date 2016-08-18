99d4762341eee79aa1c58e67c8902171d5594030
John Yale <john.yale93@gmail.com>
2016-08-19 00:07:40 +0200

    Did alot, hyperlinks, tabs, etc, layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
+  get "home" => "pages#home" # creates home_path
+  get "about" => "pages#about" # creates about_path
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
