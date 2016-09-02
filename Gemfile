b407a914f0fb48c558e23358da4a2c6bfe3227e6
John Yale <john.yale93@gmail.com>
2016-09-02 14:57:31 +0200

    Amazon S3 added

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk', '<2.0'
 
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,15 @@ Rails.application.configure do
 
    # required for heroku
    # set to actual host
-config.action_mailer.default_url_options = { host: 'pinterestingprojecthz.com'}end
+config.action_mailer.default_url_options = { host: 'pinterestingprojecthz.com'}
+
+   # Set Paperclip to upload images to Amazon S3
+   config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
+end
