4ea4fb2e07acc37dbbb4f96f36a2263166f0e304
John Yale <john.yale93@gmail.com>
2016-09-02 15:14:20 +0200

    s3 bucketname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,17 +84,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-   # required for heroku
-   # set to actual host
-config.action_mailer.default_url_options = { host: 'pinterestingprojecthz.com'}
+  # required for heroku
+  # set to actual host
+  config.action_mailer.default_url_options = { host: 'pinterestingprojecthz.com'}
 
-   # Set Paperclip to upload images to Amazon S3
-   config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => ENV['AWS_BUCKET'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-    }
+  # Set Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
