2e6d7c2f27c8bd2387dd3e548db51a229261b733
John Yale <john.yale93@gmail.com>
2016-09-07 16:46:27 +0200

    changed views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :pins
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "home" => "pages#home" # creates home_path
   get "about" => "pages#about" # creates about_path
   get "index" => "pins#index"
