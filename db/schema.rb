6a0108c8072d5440754d7540b3cfb79e9318a689
John Yale <john.yale93@gmail.com>
2016-09-07 15:47:49 +0200

    username stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831212305) do
+ActiveRecord::Schema.define(version: 20160907132405) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160831212305) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
